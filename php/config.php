0907e8cf73f7b513576a1ab9635941aa2754f271
PabloDJesus7Ca <pabloiaspasiago@icloud.com>
2025-07-31 17:00:25 -0400

    Feat: Bux Fixed

--- php/config.php
+++ php/config.php
@@ -1,18 +1,8 @@
 <?php
-
 require_once __DIR__ . '/../vendor/autoload.php';
 
 $dotenv = Dotenv\Dotenv::createImmutable(__DIR__.'/../');
 $dotenv->load();
-
-
-
-
-    
-    $host = $parsed['host'];
-    $user = $parsed['user'];
-    $password = $parsed['pass'];
-    $port = $parsed['port'] ?? 3306;
 
 
     $host = $_ENV['DB_HOST'] ?? getenv('DB_HOST') ?? 'localhost';
@@ -20,7 +10,6 @@ $dotenv->load();
     $password = $_ENV['DB_PASSWORD'] ?? getenv('DB_PASSWORD') ?? '';
     $dbname = $_ENV['DB_NAME'] ?? getenv('DB_NAME') ?? 'dblibreria';
     $port = $_ENV['DB_PORT'] ?? getenv('DB_PORT') ?? 3306;
-
 
 try {
     $dsn = "mysql:host=$host;port=$port;dbname=$dbname;";
